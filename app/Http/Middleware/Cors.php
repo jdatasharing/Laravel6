b1e8c540a77cd6b7feca5d90710dc209afc02a9b
jdatasharing <52650623+jdatasharing@users.noreply.github.com>
2020-08-08 01:03:32 +0530

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,18 +15,25 @@ class Cors
      */
    public function handle($request, Closure $next)
 {
-    $allowedOrigins = [
-        'http://localhost:3000',
-    ];
-    $requestOrigin = $request->headers->get('origin');
-
-    if (in_array($requestOrigin, $allowedOrigins)) {
-          header('Access-Control-Allow-Origin: *');
+     
+       header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
             return $next($request);
-    }
 
-    return $next($request);
+    // $allowedOrigins = [
+    //     'http://localhost:3000',
+    //     'http://192.168.0.5:4002/'
+    // ];
+    // $requestOrigin = $request->headers->get('origin');
+
+    // if (in_array($requestOrigin, $allowedOrigins)) {
+    //     return $next($request)
+    //         ->header('Access-Control-Allow-Origin', $requestOrigin)
+    //         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    //         ->header('Access-Control-Allow-Credentials', 'true')
+    //         ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+
 }
 }
